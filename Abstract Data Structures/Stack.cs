7f21a4862d12e1a0a2080cb66bfb1e1441c477bb
Ankit Singh <ankit.singh77@gmail.com>
2018-07-18 21:48:04 +0530

    Adding stack.cs

---
+++ Abstract Data Structures/Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Data_Structures
+{
+    public class Stack<T>
+    {
+        public Node<T> Top;
+
+        public Stack()
+        {
+            Top = null;
+        }
+
+        public void Push(T element)
+        {
+            Node<T> temp = new Node<T>();
+            temp.NodeContent = element;
+            if(Top == null)
+            {
+                Top = temp;
+                return;
+            }
+            temp.nextNode = Top;
+            Top = temp;
+        }
+
+        public T Pop()
+        {
+            T returnValue;
+            if (Top == null)
+            {
+                throw new Exception("Stack is empty");
+            }
+            else if (Top.nextNode == null)
+            {
+                returnValue = Top.NodeContent;
+                Top = null;
+            }
+            else
+            {
+                returnValue = Top.NodeContent;
+                Top = Top.nextNode;
+            }
+            return returnValue;
+        }
+
+        public bool IsEmpty()
+        {
+            return Top == null;
+        }
+    }
+}
---
+++ Problems/Stack/StackApplication.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADS = Abstract_Data_Structures;
+
+namespace Problems.Stack
+{
+    public static class StackApplication
+    {
+        private const char OpenParantheSis = '(';
+        private const char CloseParantheSis = ')';
+        private const char OpenSquareBracket = '[';
+        private const char CloseSquareBracket = ']';
+        private const char OpenBraces = '{';
+        private const char CloseBraces = '}';
+
+
+        public static bool BalancingSymbols(string expression)
+        {
+            ADS.Stack<char> charactersStack = new ADS.Stack<char>();
+            foreach (char item in expression)
+            {
+                switch(item)
+                {
+                    case OpenParantheSis:
+                    case OpenSquareBracket:
+                    case OpenBraces:
+                        {
+                            charactersStack.Push(item);
+                            break;
+                        }
+                    case CloseParantheSis:
+                        {
+                            if(charactersStack.Pop()!=OpenParantheSis)
+                            {
+                                return false;
+                            }
+                            break;
+                        }
+                    case CloseSquareBracket:
+                        {
+                            if(charactersStack.Pop()!= OpenSquareBracket)
+                            {
+                                return false;
+                            }
+                            break;
+                        }
+                    case CloseBraces:
+                        {
+                            if(charactersStack.Pop()!=OpenBraces)
+                            {
+                                return false;
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+            }
+            return charactersStack.IsEmpty();
+        }
+    }
+}
