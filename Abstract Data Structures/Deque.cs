f6d014bf4cf32b839f3df7ce4bb4bc496596c08a
Ankit Singh <sesa489457@se.com>
2021-08-15 12:07:18 +0530

    Addition of Deque data structure

---
+++ Abstract Data Structures/Deque.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Abstract_Data_Structures.Interfaces;
+
+namespace Abstract_Data_Structures
+{
+    /// <summary>
+    /// Double Ended Queue Linked List Implementation
+    /// </summary>
+    public class Deque<T> : IDeque<T>
+    {
+        class Node
+        {
+            public T Value { get; set; }
+            public Node Next { get; set; }
+            public Node Prev { get; set; }
+        }
+
+        private Node _front = null;
+        private Node _rear = null;
+        private int _size = 0;
+
+        int IDeque<T>.Count => _size;
+
+        public void OfferFirst(T element)
+        {
+            var node = new Node() { Value = element};
+            if (_front == null)
+            {
+                _front = node;
+                _rear = node;
+            }
+            else
+            {
+                _front.Prev = node;
+                node.Next = _front;
+                _front = node;
+            }
+            _size++;
+        }
+
+        public void OfferLast(T element)
+        {
+            var node = new Node() { Value = element};
+            if (_rear == null)
+            {
+                _rear = node;
+                _front = node;
+            }
+            else
+            {
+                node.Prev = _rear;
+                _rear.Next = node;
+                _rear = node;
+            }
+
+            _size++;
+        }
+
+        public void PollFirst()
+        {
+            if (_front == null)
+            {
+                throw new Exception("Empty Deque, cannot poll");
+            }
+
+            if (_size == 1)
+            {
+                _front = null;
+                _rear = null;
+            }
+            else
+            {
+                _front.Next.Prev = null;
+                var node = _front;
+                _front = _front.Next;
+                node.Next = null;
+            }
+
+            _size--;
+        }
+
+        public void PollLast()
+        {
+            if (_rear == null)
+            {
+                throw new Exception("Empty Deque, cannot poll");
+            }
+
+            if (_size == 1)
+            {
+                _front = null;
+                _rear = null;
+            }
+            else 
+            {
+                var node = _rear.Prev;
+                _rear.Prev = null;
+                node.Next = null;
+                _rear = node;
+            }
+
+            _size--;
+        }
+
+        public T PeekFirst()
+        {
+            if (_front == null)
+            {
+                throw new Exception("Invalid Operation, Queue is empty");
+            }
+
+            return _front.Value;
+        }
+
+        public T PeekLast()
+        {
+            if (_rear == null)
+            {
+                throw new Exception("Invalid Operation, Queue is empty");
+            }
+
+            return _rear.Value;
+        }
+
+        bool IDeque<T>.IsEmpty()
+        {
+            return _size == 0;
+        }
+    }
+
+
+}
---
+++ Abstract Data Structures/Interfaces/IDeque.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Data_Structures.Interfaces
+{
+    public interface IDeque<T>
+    {
+        /// <summary>
+        /// Insert at Begining.
+        /// </summary>
+
+        void OfferFirst(T element);
+
+        /// <summary>
+        /// Insert at End.
+        /// </summary>
+        /// <param name="element"></param>
+        void OfferLast(T element);
+
+        /// <summary>
+        /// Delete from Fisrt
+        /// </summary>
+        void PollFirst();
+
+        /// <summary>
+        /// Delete from Last
+        /// </summary>
+        void PollLast();
+
+        /// <summary>
+        /// Front Element.
+        /// </summary>
+        /// <returns></returns>
+        T PeekFirst();
+
+        /// <summary>
+        /// Rear Element.
+        /// </summary>
+        /// <returns></returns>
+        T PeekLast();
+
+        /// <summary>
+        /// Check if the queue is empty or not.
+        /// </summary>
+        /// <returns></returns>
+        bool IsEmpty();
+
+        /// <summary>
+        /// Return the size of queue.
+        /// </summary>
+        int Count { get; }
+    }
+}
